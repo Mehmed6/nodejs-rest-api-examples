c69cf77ca141a290e83c407d124a602094c1d5e1
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2026-01-03 02:08:18 +0100

    Add initial postal code schema
    Create postal_codes and postal_code_info tables.
    
    Includes primary keys, foreign key relationship,
    and basic location and administrative fields.

---
+++ 002-DemoGeonamesPostalCodeSearch/ddl.sql
@@ -0,0 +1,17 @@
+create table if not exists postal_codes (
+    code varchar(100) primary key,
+    record_date_time timestamp default(current_timestamp) not null
+    );
+
+create table if not exists postal_code_info (
+    postal_code_info_id bigserial primary key,
+    postal_code varchar(100) references postal_codes(code) not null,
+    adminCode2 varchar(250),
+    adminCode1 varchar(250),
+    adminName2 varchar(250),
+    lng double precision not null,
+    lat double precision not null,
+    countryCode char(3) not null,
+    adminName1 varchar(250),
+    placeName varchar(250)
+    );
